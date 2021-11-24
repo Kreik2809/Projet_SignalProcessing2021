ac922504e726f02f7eb722d98143ce380b26a8fb
Kreik2809 <200098@umons.ac.be>
2021-11-24 16:52:35 +0100

    add pitch1 + sdl data

--- src/main/utils.py
+++ src/main/utils.py
@@ -2,15 +2,24 @@ import audiofile
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
+import glob, os, random
+
 
 """
-   Read a given wav audio file and return the signal of it and its sampling rate. 
+    Read a given wav audio file and return the signal of it and its sampling rate.
+      @path : path to the .wav file [string]
+
+      @return : the signal [ndarray] and the sampling rate of the .wav file [int]
 """
 def read_wavfile(path):
     signal, sampling_rate = audiofile.read(path)
     return signal, sampling_rate
+
 """
     Normalize a signal in order to make his value ranges from -1 to 1.
+      @signal : the signal [ndarray]
+
+      @return : the normalized signal [ndarray]
 """
 def normalize(signal):
     min_value = abs(min(signal)) 
@@ -20,8 +29,10 @@ def normalize(signal):
 
     return signal/norm
 """
-    window_width : [ms]
-    sliding_step : [ms]
+    Normalize a signal in order to make his value ranges from -1 to 1.
+      @signal : the signal [ndarray]
+
+      @return : the normalized signal [ndarray]
 """
 def split(signal, sampling_rate, window_width, sliding_step):
     window_samples = sampling_rate * (window_width/1000)
@@ -36,10 +47,8 @@ def split(signal, sampling_rate, window_width, sliding_step):
         if is_window:
             if count < window_samples:
                 current_win.append(signal[i])
-                count = count + 1 
+                count += 1
             else:
-                print("J'arrête la window en :")
-                print(i)
                 windows.append(current_win)
                 current_win = []
                 count = 0
@@ -49,8 +58,6 @@ def split(signal, sampling_rate, window_width, sliding_step):
             if count < sliding_samples:
                 count += 1
             else:
-                print("J'arrête la step")
-                print(i)
                 count = 0
                 is_window = True
                 is_step = False
@@ -62,6 +69,47 @@ def split(signal, sampling_rate, window_width, sliding_step):
 
     return windows
 
-signal, sampling_rate = read_wavfile("../data/arctic_a0001.wav")
+"""
+    Return the energy of the given signal
+"""
+def compute_energy(signal):
+    energy = 0
+    for i in range(len(signal)):
+        energy += (abs(signal[i]))**2
+    return energy
+
+
+"""
+    path of the directory where utterances are stored
+"""
+def auto_correlation_pitch_estim(path):
+    #1.
+    os.chdir(path)
+    files = glob.glob("*.wav")
+    
+    choosed_files = random.sample(files, 5)
+    signal = []
+    for file in choosed_files:
+        current_signal, sampling_rate = read_wavfile(file)
+        signal.extend(current_signal)
+
+    #2.
+    signal = normalize(signal)
+    plt.subplot(211)
+    plt.plot(signal)
+    
+    #3.
+    frames = split(signal, sampling_rate, 50, 0)
+    list_energies = []
+    for s in frames:
+        list_energies.append(compute_energy(s))
+    plt.subplot(212)
+    plt.plot(list_energies)
+    plt.show()
+
+auto_correlation_pitch_estim("../../data/bdl")
+
+
+
 
 #data/arctic_a0001.wav
