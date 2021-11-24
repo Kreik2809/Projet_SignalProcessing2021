898927ef8409272e5b74cca273c074244999e651
Kreik2809 <200098@umons.ac.be>
2021-11-24 14:11:02 +0100

    normalizeV2

--- src/utils.py
+++ src/utils.py
@@ -4,19 +4,27 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 """
-   Read a wav audio file and return the signal of it and its sampling rate. 
+   Read a given wav audio file and return the signal of it and its sampling rate. 
 """
 def read_wavfile(path):
-    signal, sampling_rate = audiofile.read("data/arctic_a0001.wav")
-    return signal, sampling
+    signal, sampling_rate = audiofile.read(path)
+    return signal, sampling_rate
 """
     Normalize a signal in order to make his value range from -1 to 1.
 """
-def normalize(signal):
-    norm = np.linalg.norm(signal)
-    signal = signal/norm
-    return signal   
+def normalize_1(signal):
+    min_value = abs(min(signal)) 
+    max_value = abs(max(signal))
+
+    norm = max(min_value, max_value)
+
+    return signal/norm
+
+signal, sampling_rate = read_wavfile("data/arctic_a0001.wav")
 
 
-plt.plot(signal)
+plt.plot(normalize(signal))
+
 plt.show()
+
+#data/arctic_a0001.wav
