f2e734c886e9884554cc19d008872c8bbdd1fe66
Kreik2809 <200098@umons.ac.be>
2021-11-24 14:37:19 +0100

    normalize

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,7 @@ def read_wavfile(path):
     signal, sampling_rate = audiofile.read(path)
     return signal, sampling_rate
 """
-    Normalize a signal in order to make his value range from -1 to 1.
+    Normalize a signal in order to make his value ranges from -1 to 1.
 """
 def normalize(signal):
     min_value = abs(min(signal)) 
@@ -19,6 +19,21 @@ def normalize(signal):
     norm = max(min_value, max_value)
 
     return signal/norm
+"""
+    window_width : [ms]
+    sliding_step : [ms]
+"""
+def split(signal, sampling_rate, window_width, sliding_step):
+    window_samples = sampling_rate * (window_width/1000)
+    sliding_samples = sampling_rate * (sliding_step/1000)
+
+    windows = []
+    count = 0
+    current_win = []
+    for i in range(len(signal)):
+            current_win.append(signal[i]
+
+
 
 signal, sampling_rate = read_wavfile("data/arctic_a0001.wav")
 
