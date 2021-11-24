1ab352b45957291ed9d9013f60de22291624bbcd
Kreik2809 <200098@umons.ac.be>
2021-11-24 13:50:33 +0100

    initial commit

---
+++ src/filterbanks.py
@@ -0,0 +1,36 @@
+# can be imported from the labs directory
+import numpy
+
+
+def filter_banks(pow_frames, sample_rate, nfilt = 40, NFFT=512):
+    """calculate filter banks.
+
+    Args:
+        pow_frames (np.array): frames of the power spectrum, one frame per row
+        sample_rate (numerical): sampling frequency
+        nfilt (numerical): number of filters in filterbank. Defaults to 40.
+        NFFT (numerical):  number of frequency calculated, typically 512. Defaults to 512.
+
+    Returns:
+        [np.array]: filter banks in db
+    """
+    low_freq_mel = 0
+    high_freq_mel = (2595 * numpy.log10(1 + (sample_rate / 2) / 700))  # Convert Hz to Mel
+    mel_points = numpy.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # Equally spaced in Mel scale
+    hz_points = (700 * (10**(mel_points / 2595) - 1))  # Convert Mel to Hz
+    bin = numpy.floor((NFFT + 1) * hz_points / sample_rate)
+
+    fbank = numpy.zeros((nfilt, int(numpy.floor(NFFT / 2 + 1))))
+    for m in range(1, nfilt + 1):
+        f_m_minus = int(bin[m - 1])   # left
+        f_m = int(bin[m])             # center
+        f_m_plus = int(bin[m + 1])    # right
+
+        for k in range(f_m_minus, f_m):
+            fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+        for k in range(f_m, f_m_plus):
+            fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+    filter_banks = numpy.dot(pow_frames, fbank.T)
+    filter_banks = numpy.where(filter_banks == 0, numpy.finfo(float).eps, filter_banks)  # Numerical Stability
+    filter_banks = 20 * numpy.log10(filter_banks)  # dB
+    return filter_banks
---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import audiofile
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+"""
+    
+"""
+def read_wavfile(path):
+    signal, sampling_rate = audiofile.read("data/arctic_a0001.wav")
+    return signal, sampling
+
+def
+
+
+plt.plot(signal)
+plt.show()
