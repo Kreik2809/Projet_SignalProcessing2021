96603e4c74c0b819d2c6972fece889b96aeb5de7
Kreik2809 <200098@umons.ac.be>
2021-11-29 16:46:46 +0100

    Unit test for split and normalize

--- src/tests/normalize_test.py
+++ src/tests/normalize_test.py
@@ -2,18 +2,34 @@ import sys
 sys.path.append("..")
 sys.path.append("../main")
 from main import utils
+import unittest
+import random
+import numpy as np
 
-signal, sampling_rate = utils.read_wavfile("../../data/bdl_a/arctic_a0001.wav")
 
-windows = utils.split(signal, sampling_rate, 1000, 500)
- 
-#Test 1
-w1 = signal[:16000]
-print((w1 == windows[0]).all())
- 
-#Test 2
-w2 = signal[24000:40000]
-print((w2 == windows[1]).all()) 
+class Normalize_test(unittest.TestCase):
+    
+
+    """
+        Unit test to test if the max value of the normalized array is <= 1
+    """
+    def test_max(self):
+        array = np.array([random.uniform(-10, 10) for p in range(10)])
+        n_array = utils.normalize(array)
+        maximum = max(n_array)
+        self.assertTrue(maximum <= 1, "")
+    
+    """
+        Unit test to test if the min value of the normalized array is >= -1
+    """
+    def test_min(self):
+        array = np.array([random.uniform(-10, 10) for p in range(10)])
+        n_array = utils.normalize(array)
+        minimum = min(n_array)
+        self.assertTrue(minimum >= -1, "")
+
+if __name__ == "__main__":
+   unittest.main() 
 
 
 
--- src/tests/split_test.py
+++ src/tests/split_test.py
@@ -1,3 +1,45 @@
 import sys
-sys.path.insert(1, '../')
-sys.path.insert(1, '../src')
+sys.path.append("..")
+sys.path.append("../main")
+from main import utils
+import unittest
+import random
+import numpy as np
+
+
+class Split_test(unittest.TestCase):
+        
+    """
+        Unit test to test split function with sliding step equals to frame size
+    """
+    def test_01(self):
+        tab = np.array([random.uniform(-10, 10) for p in range(100)])
+        windows = utils.split(tab, 1000, 20, 20)
+        self.assertTrue((tab[:20] == windows[0]).all(), "First frame test")
+        self.assertTrue((tab[20:40] == windows[1]).all(), "Second frame test")
+        self.assertFalse((tab[10:30] == windows[0]).all(), "Wrong frame test")
+
+    """
+        Unit test to test split function with sliding step < thant frame size
+    """
+    def test_02(self):
+        tab = np.array([random.uniform(-10, 10) for p in range(100)])
+        windows = utils.split(tab, 1000, 20, 10)
+        self.assertTrue((tab[:20] == windows[0]).all(), "First frame test")
+        self.assertTrue((tab[10:30] == windows[1]).all(), "Second frame test")
+        self.assertFalse((tab[20:40] == windows[1]).all(), "Wrong frame test")
+
+    def test_03(self):
+        tab = np.array([random.uniform(-10, 10) for p in range(100)])
+        windows = utils.split(tab, 1000, 20, 30)
+        self.assertTrue((tab[:20] == windows[0]).all(), "First frame test")
+        self.assertTrue((tab[30:50] == windows[1]).all(), "Second frame test")
+        self.assertFalse((tab[20:40] == windows[1]).all(), "Wrong frame test")
+        
+if __name__ == "__main__":
+   unittest.main()      
+
+ 
+
+
+
